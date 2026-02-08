ee02b7c54f0afedbe4e9229deea75b4d47e52573
jakobakoba <jakobakoba@gmail.com>
2026-02-09 04:29:08 +0900

    feat: добавил зависимости камеры

--- feature/record/build.gradle.kts
+++ feature/record/build.gradle.kts
@@ -50,4 +50,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.camera.core)
+    implementation(libs.camera.camera)
+    implementation(libs.camera.lifecycle)
+    implementation(libs.camera.video)
+    implementation(libs.camera.view)
+    implementation(libs.camera.extensions)
 }
