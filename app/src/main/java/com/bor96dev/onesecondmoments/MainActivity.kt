d3a8e6f8bb60fc832b571902381d2a490125ee3c
jakobakoba <jakobakoba@gmail.com>
2026-02-08 23:43:39 +0900

    feat: добавил навигацию между экранами

--- app/src/main/java/com/bor96dev/onesecondmoments/MainActivity.kt
+++ app/src/main/java/com/bor96dev/onesecondmoments/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.bor96dev.onesecondmoments.navigation.NavigationRoot
 import com.bor96dev.onesecondmoments.ui.theme.OneSecondMomentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OneSecondMomentsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavigationRoot()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OneSecondMomentsTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/bor96dev/onesecondmoments/navigation/NavigationRoot.kt
@@ -0,0 +1,127 @@
+package com.bor96dev.onesecondmoments.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import androidx.savedstate.serialization.SavedStateConfiguration
+import com.bor96dev.calendar.CalendarScreen
+import com.bor96dev.montage.MontageScreen
+import com.bor96dev.record.RecordScreen
+import com.bor96dev.ui.R
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+
+@Composable
+fun NavigationRoot(
+    modifier: Modifier = Modifier
+) {
+    val backStack = rememberNavBackStack(
+        configuration = SavedStateConfiguration {
+            serializersModule = SerializersModule {
+                polymorphic(NavKey::class) {
+                    subclass(Route.Record::class, Route.Record.serializer())
+                    subclass(Route.Montage::class, Route.Montage.serializer())
+                    subclass(Route.Calendar::class, Route.Calendar.serializer())
+                }
+            }
+        },
+        Route.Record
+    )
+
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            NavigationBar {
+                val currentRoute = backStack.last()
+                NavigationBarItem(
+                    selected = currentRoute is Route.Calendar,
+                    onClick = {
+                        if (currentRoute !is Route.Calendar) {
+                            backStack.clear()
+                            backStack.add(Route.Calendar)
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.calendar),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text("Calendar") }
+                )
+
+                NavigationBarItem(
+                    selected = currentRoute is Route.Record,
+                    onClick = {
+                        if (currentRoute !is Route.Record) {
+                            backStack.clear()
+                            backStack.add(Route.Record)
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.record),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text("Record") }
+                )
+
+                NavigationBarItem(
+                    selected = currentRoute is Route.Montage,
+                    onClick = {
+                        if (currentRoute !is Route.Montage) {
+                            backStack.clear()
+                            backStack.add(Route.Montage)
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.montage),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text("Montage") }
+                )
+            }
+        }
+    ) { padding ->
+        NavDisplay(
+            modifier = Modifier.padding(padding),
+            backStack = backStack,
+            entryProvider = { key ->
+                when (key) {
+                    is Route.Record -> {
+                        NavEntry(key) {
+                            RecordScreen()
+                        }
+                    }
+
+                    is Route.Montage -> {
+                        NavEntry(key) {
+                            MontageScreen()
+                        }
+                    }
+
+                    is Route.Calendar -> {
+                        NavEntry(key) {
+                            CalendarScreen()
+                        }
+                    }
+
+                    else -> error("Unknown NavKey: $key")
+                }
+            }
+        )
+    }
+}
