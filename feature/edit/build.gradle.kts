7c6cb8d5823893365dfea247f164c9879ae97352
jakobakoba <jakobakoba@gmail.com>
2026-02-11 01:50:56 +0900

    feat: добавил exoplayer dependencies

--- feature/edit/build.gradle.kts
+++ feature/edit/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -39,4 +40,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+    implementation(libs.exoplayer)
+    implementation(libs.exoplayer.ui)
+
 }
