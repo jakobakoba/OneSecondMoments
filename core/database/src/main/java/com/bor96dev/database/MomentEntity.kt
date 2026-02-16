86401f62dc654ca9de841ae3fae6c5a04e015b5f
jakobakoba <jakobakoba@gmail.com>
2026-02-16 18:30:41 +0900

    feat: создал momentEntity

---
+++ core/database/src/main/java/com/bor96dev/database/MomentEntity.kt
@@ -0,0 +1,10 @@
+package com.bor96dev.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "moments")
+data class MomentEntity (
+    @PrimaryKey val date: String,
+    val videoUri: String,
+)
