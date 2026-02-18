61a2becf36e02871a86c435a52bae4e00c5f1065
jakobakoba <jakobakoba@gmail.com>
2026-02-19 00:37:13 +0900

    feat: заглушка текст glue screen

--- feature/glue/src/main/java/com/bor96dev/glue/presentation/GlueScreen.kt
+++ feature/glue/src/main/java/com/bor96dev/glue/presentation/GlueScreen.kt
@@ -1,8 +1,24 @@
 package com.bor96dev.glue.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun GlueScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
 
+        Text("GlueScreen")
+
+
+    }
 }
