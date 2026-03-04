861fd1c62d257d882c9f20e433f2124c46fd317e
jakobakoba <jakobakoba@gmail.com>
2026-03-04 22:28:27 +0900

    feat: вынос логики вычисления дней

---
+++ feature/montage/src/main/java/com/bor96dev/montage/presentation/stats/MontageStatsCalculator.kt
@@ -0,0 +1,25 @@
+package com.bor96dev.montage.presentation.stats
+
+import com.bor96dev.database.MomentEntity
+import com.bor96dev.montage.presentation.state.MonthStat
+import com.bor96dev.montage.presentation.state.YearStat
+import java.time.LocalDate
+import java.time.YearMonth
+
+object MontageStatsCalculator {
+    fun computeYearlyStats(moments: List<MomentEntity>): List<YearStat> {
+        return moments
+            .groupBy { LocalDate.parse(it.date).year }
+            .map { (year, list) -> YearStat(year, list.size) }
+            .sortedByDescending { it.year }
+    }
+
+    fun computeMonthlyStats(moments: List<MomentEntity>): List<MonthStat> {
+        return moments
+            .groupBy {
+                YearMonth.parse(it.date.substring(0, 7))
+            }
+            .map { (yearMonth, list) -> MonthStat(yearMonth, list.size) }
+            .sortedByDescending { it.yearMonth }
+    }
+}
