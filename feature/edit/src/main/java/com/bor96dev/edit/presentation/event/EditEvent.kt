c2967e2a09ff0f34013a2886b458b56bba0c62ef
jakobakoba <jakobakoba@gmail.com>
2026-03-04 20:27:00 +0900

    Feat: geotag (#23)
    * feat: permission для гео
    
    * feat: locationText
    
    * feat: разрешения и логика добавления гео

--- feature/edit/src/main/java/com/bor96dev/edit/presentation/event/EditEvent.kt
+++ feature/edit/src/main/java/com/bor96dev/edit/presentation/event/EditEvent.kt
@@ -4,8 +4,8 @@ import android.net.Uri
 
 
 sealed interface EditEvent {
-    data class OnVideoLoaded(val uri: Uri, val duration: Long): EditEvent
     data class OnSeekChanged(val positionMs: Long) : EditEvent
+    data class LocationPermissionResult(val granted: Boolean) : EditEvent
     object TogglePlay: EditEvent
     object SaveClicked: EditEvent
     object OnBackClicked: EditEvent
